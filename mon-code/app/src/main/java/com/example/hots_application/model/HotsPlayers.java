baed8ae28ef9178e9d6b6eff62373beff3c7794d
Quentin Simon <quentin.simon0821@gmail.com>
2019-06-05 19:57:36 +0200

    Creation de Hotsplayers

---
+++ mon-code/app/src/main/java/com/example/hots_application/model/HotsPlayers.java
@@ -0,0 +1,44 @@
+package com.example.hots_application.model;
+
+public class HotsPlayers {
+    private  String name;
+    private  String short_name;
+    private  String attribute_id;
+    private  String icon_url;
+    private  String role;
+    private  String type;
+    private  String release_date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShort_name() {
+        return short_name;
+    }
+
+    public String getAttribute_id() {
+        return attribute_id;
+    }
+
+    public String getUrl() {
+        return icon_url;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getrealise() {
+        return release_date;
+    }
+
+}
