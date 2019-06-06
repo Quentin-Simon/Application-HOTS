2c3c791a8e99b4bd41c192fe2f5499a896a73874
Quentin Simon <quentin.simon0821@gmail.com>
2019-06-06 10:20:52 +0200

    Actualisation appel serveur

--- mon-code/app/src/main/java/com/example/hots_application/HotsPlayersRestApi.java
+++ mon-code/app/src/main/java/com/example/hots_application/HotsPlayersRestApi.java
@@ -1,11 +1,13 @@
 package com.example.hots_application;
 
-import com.example.hots_application.model.RestHotsPlayersReponse;
+import com.example.hots_application.model.HotsPlayers;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface HotsPlayersRestApi {
     @GET("heroes/")
-    Call<RestHotsPlayersReponse> getHotsPlayersList();
+    Call<List<HotsPlayers>> getHotsPlayersList();
 }
