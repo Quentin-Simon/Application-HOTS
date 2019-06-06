f451f62666075aa08547ded33fc5a25f5f53d50c
Quentin Simon <quentin.simon0821@gmail.com>
2019-06-06 09:53:46 +0200

    Internet Permission

--- mon-code/app/src/main/java/com/example/hots_application/HotsPlayersRestApi.java
+++ mon-code/app/src/main/java/com/example/hots_application/HotsPlayersRestApi.java
@@ -6,6 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface HotsPlayersRestApi {
-    @GET("HotsPlayers/")
+    @GET("heroes/")
     Call<RestHotsPlayersReponse> getHotsPlayersList();
 }
